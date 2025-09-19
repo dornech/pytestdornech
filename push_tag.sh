98ab255f5f6bfc55a14ac916cc153c71e2bdeaea
dornech <dornech@users.noreply.github.com>
2025-09-19 23:43:17 +0200

    fix: correct commitizen post_bump_hook /3.
    correct commitizen post_bump_hook

--- push_tag.sh
+++ push_tag.sh
@@ -3,6 +3,3 @@
 
 # push git with version tag to GitHub to initiate GitHub workflow
 git push origin $CZ_POST_CURRENT_TAG_VERSION
-echo $CZ_POST_CURRENT_TAG_VERSION
-
-env > post_bump_hook_output.txt
